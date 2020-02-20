ebbfec9b9bd56ecd28f840122f30bf1ce99f7677
mijung-in-11st <mijung.ko.11st@gmail.com>
2020-02-20 10:06:32 +0900

    complete

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- 
+    
+      컴포넌트 명명법
+    <hello-world></hello-world>
+    <HelloWorld></HelloWorld>
+    <HelloWorld/> : 가장 진보된 명명법
+    
+  -->
+  <div>
+    <!-- root는 하나만 있어야함 -->
+    {{ str }}
+    <!-- v-bind:프롭스 속성이름(하위 컴포넌트)="상위 컴포넌트 데이터이름"-->
+    <app-header 
+      v-bind:propsdata="str"
+      v-on:renew="renewStr"></app-header>
+  </div>  
+</template>
+
+<script>
+// import 변수명 from '파일 위치';
+// 파일 내용을 변수에 담음
+import AppHeader from './components/AppHeader.vue';
+
+// var AppHeader = {
+//   template: '<header><h1>Header</h1></header>'
+// }
+
+export default {
+  //JavaScript - component option
+  // function으로 만들고 객체 반환
+  data: function() {
+    return {
+      str: 'Header'
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'hi';
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue-cli/src/a.vue
@@ -0,0 +1,32 @@
+<!-- vue file 이해하기 -->
+<!-- vue 하고 tap 하면 기본구조 -->
+
+// var appHeader = {
+//     template: '<div>header</div>',
+//     methods: {
+//         addNum: function() {
+            
+//         }
+//     }
+// }
+// 이거를 vue에서는 아래와 같이 구현함
+
+<template>
+    <!-- HTML -->
+    <div>header</div>
+</template>
+
+<script>
+export default {
+    // Javascript - instance option
+    methods: {
+        addNum: function() {
+
+        }
+    }
+}
+</script>
+
+<style>
+    /* CSS */
+</style>
